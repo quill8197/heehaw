8bc1a4702e49c83b165829a88dd1fe1df7a0d409
Quill McConnell <quill8197@yahoo.com>
2019-04-18 01:39:01 -0400

    Added validation and basic functionality with tests

--- heehawfunction.js
+++ heehawfunction.js
@@ -1,4 +1,34 @@
-function heeHaw($number)
+function heeHaw($max)
 {
-
+    console.log($max + ' ' + typeof($max));
+    if (typeof($max) === 'number') //Number.isInteger($max)
+    {
+        for (let i = 1; i <= $max; i++)
+        {
+            if (i % 3 === 0 && i % 5 === 0)
+            {
+                console.log("Hee Haw");
+            }
+            else if (i % 3 === 0)
+            {
+                console.log("Hee");
+            }
+            else if (i % 5 === 0)
+            {
+                console.log("Haw");
+            }
+            else
+            {
+                console.log(i.toString());
+            }
+        }
+    }
+    else
+    {
+        console.log("Error: Invalid input type. Input must be a number.");
+    }
 }
+
+//heeHaw(5);
+//heeHaw('six');
+heeHaw(-5);
