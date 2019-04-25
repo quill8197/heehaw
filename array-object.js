42279a6099fda3c133c35aacd78c69d5ea81cc80
Quill McConnell <quill8197@yahoo.com>
2019-04-25 01:36:47 -0400

    Added a function that takes an array of values and sorts them into an object separated by type

---
+++ array-object.js
@@ -0,0 +1,55 @@
+function groupByType(array)
+{
+    /*
+    let objectA = {a: 1, b: 2};
+    Object.assign(objectA, {b: 3, c: 4});
+    console.log(objectA);
+    // → {a: 1, b: 3, c: 4}
+
+    function addEntry(events, squirrel)
+    {
+      journal.push({events, squirrel});
+    }
+
+    addEntry(["work", "touched tree", "pizza", "running",
+          "television"], false);
+    */
+    //create arrays to track strings, numbers, and booleans
+    let strings = [];
+    let numbers = [];
+    let booleans = [];
+
+    //store the array's element's in the placeholder arrays by type
+    for (let i = 0; i < array.length; i++)
+    {
+        if (typeof array[i] === 'string')
+        {
+            strings.push(array[i]);
+        }
+        else if (typeof array[i] === 'number')
+        {
+            numbers.push(array[i]);
+        }
+        else
+        {
+            booleans.push(array[i]);
+        }
+    }
+
+    //convert the array into an object with properties containing the array values, grouped by type
+    let object = {};
+    Object.assign(object, {
+        strings: strings,
+        numbers: numbers,
+        booleans: booleans
+    });
+
+    //return an object
+    return object;
+}
+
+//test the function
+let input = [ -1, 5, "cat", false, 10.2, true, "dog" ];
+
+let object = groupByType(input);
+console.log(object);
