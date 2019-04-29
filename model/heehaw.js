da2f6253e164050caa88e78930355e967b8a7dcd
Quill McConnell <quill8197@yahoo.com>
2019-04-29 16:34:32 -0400

    Completed the in-class exercise

--- model/heehaw.js
+++ model/heehaw.js
@@ -1,3 +1,41 @@
+/**
+ // Attach an event handler to the button
+ var btn = document.getElementById("go");
+ btn.onclick = heeHaw;
+
+ function heeHaw()
+ {
+        // Get the input
+        var num = document.getElementById("number").value;
+        num = parseInt(num);
+
+        // Validate input
+        if(isNaN(num) || num <= 0) {
+            output.innerHTML = "<p>Please enter a valid number.</p>";
+            return;
+        }
+
+        // Get the output div
+        var output = document.getElementById("result");
+
+        // Loop through numbers and display appropriate output
+        var text = "";
+        for(var i = 1; i <= num; i++) {
+
+            if (i % 5 === 0 && i % 3 === 0) {
+                text = "Hee Haw!";
+            } else if (i % 3 === 0) {
+                text = "Hee!";
+            } else if (i % 5 === 0) {
+                text = "Haw!";
+            } else {
+                text = i;
+            }
+            output.innerHTML += "<p>" + text + "</p>";
+        }
+    }
+ */
+
 const max = 100;
 
 for (let i = 1; i <= max; i++)
